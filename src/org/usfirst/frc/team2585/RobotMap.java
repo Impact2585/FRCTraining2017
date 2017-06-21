4c72d5893a2bd1411898ca298478bc4a934f298b
Joshua Wong <joshua99wong@gmail.com>
2017-06-21 16:12:38 -0500

    Update RobotMap.java

--- src/org/usfirst/frc/team2585/RobotMap.java
+++ src/org/usfirst/frc/team2585/RobotMap.java
@@ -7,7 +7,7 @@ public interface RobotMap {
 	// Wheels
 	// Right side has two motor controllers,left side has one
 	public static final int RIGHT_DRIVE = 1;
-	public static final int LEFT_DRIVE = 2;
+	public static final int LEFT_DRIVE = 3;
 	public static final int SOLENOID = 4;
 	public static final int INTAKE_MOTOR = 5;
 	public static final int AGITATOR = 6;
