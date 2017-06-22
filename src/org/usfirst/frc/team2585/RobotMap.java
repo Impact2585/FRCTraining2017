a1a8afb3abf136309cf7d65a36a45c75fe754e22
Ryan Knightly <reknightly@gmail.com>
2017-06-22 14:45:53 -0500

    Fixports (#17)
    * Fixing rightDrive port numbers
    
    * Update RobotMap.java

--- src/org/usfirst/frc/team2585/RobotMap.java
+++ src/org/usfirst/frc/team2585/RobotMap.java
@@ -6,8 +6,7 @@ package org.usfirst.frc.team2585;
 public interface RobotMap {
 	// Wheels
 	// Right side has two motor controllers,left side has one
-	public static final int RIGHT_UPPER_DRIVE = 1;
-	public static final int RIGHT_LOWER_DRIVE= 2;
+	public static final int RIGHT_DRIVE = 1;
 	public static final int LEFT_DRIVE = 3;
 	public static final int SOLENOID = 4;
 	public static final int INTAKE_MOTOR = 5;
