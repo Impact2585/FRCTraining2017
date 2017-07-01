c63586253862bb857c0f222d497835b9ace1d6ff
rknightly <reknightly@gmail.com>
2017-07-01 10:03:55 -0500

    lengthening auton times

--- src/org/usfirst/frc/team2585/robot/Commands.java
+++ src/org/usfirst/frc/team2585/robot/Commands.java
@@ -52,9 +52,9 @@ public class Commands {
 	 * Autonomous command that that drives the robot forward and then turns left and continues driving forward
 	 */
 	public class RightSide implements AutonomousCommand {
-		private static final int timeToDriveStraight = 1000;
-		private static final int timeToTurnLeft = timeToDriveStraight + 200;
-		private static final int timeToDriveLeft = timeToTurnLeft + 500;
+		private static final int timeToDriveStraight = 3500;
+		private static final int timeToTurnLeft = timeToDriveStraight + 400;
+		private static final int timeToDriveLeft = timeToTurnLeft + 1000;
 		
 		/* (non-Javadoc)
 		 * @see org.usfirst.frc.team2585.AutonomousCommand#execute(long)
@@ -77,9 +77,9 @@ public class Commands {
 	 * Autonomous command that drives the robot forward and then turns right and drives forward again
 	 */
 	public class LeftSide implements AutonomousCommand {
-		private static final int timeToDriveStraight = 1000;
-		private static final int timeToTurnRight = timeToDriveStraight + 200;
-		private static final int timeToDriveRight = timeToTurnRight + 500;
+		private static final int timeToDriveStraight = 3500;
+		private static final int timeToTurnRight = timeToDriveStraight + 400;
+		private static final int timeToDriveRight = timeToTurnRight + 1000;
 		
 		/* (non-Javadoc)
 		 * @see org.usfirst.frc.team2585.AutonomousCommand#execute(long)
@@ -102,7 +102,7 @@ public class Commands {
 	 * Autonomous command that drives the robot straight for 1.5 seconds and then stops
 	 */
 	public class Center implements AutonomousCommand {
-		private static final int timeToDrive = 3000;
+		private static final int timeToDrive = 3100;
 
 		@Override
 		public void execute(long timeElapsed) {			
